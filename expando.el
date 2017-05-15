0771629552ae029532c3c2e89caa09cf2695411b
Dave Pearson <davep@davep.org>
2017-05-15 10:07:34 +0100

    v1.1

--- expando.el
+++ expando.el
@@ -2,7 +2,7 @@
 ;; Copyright 2017 by Dave Pearson <davep@davep.org>
 
 ;; Author: Dave Pearson <davep@davep.org>
-;; Version: 1.0
+;; Version: 1.1
 ;; Keywords: lisp
 ;; URL: https://github.com/davep/expando.el
 
