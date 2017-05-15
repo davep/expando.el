ac604d2fba1ab1344258792de0995906139be0f6
Dave Pearson <davep@davep.org>
2017-05-15 14:41:49 +0100

    Switch to elisp--preceding-sexp to get the form to expand
    This is sort of naughty because I'm using an internal function to get the
    expression. On the other hand thing-at-point was failing me in some
    situations and the use I have for expando-macro is one where it's just like
    C-x C-e but it expands a form rather than executes it.
    
    So..... although it's naughty to use an internal I'm going to do it anyway.

--- expando.el
+++ expando.el
@@ -14,10 +14,15 @@
 ;;
 ;; expando.el provides a simple tool for expanding macros into a different
 ;; window so they can be quickly and easily checked and read.
+;;
+;; NOTE: This code makes use of `elisp--preceding-sexp' from elisp-mode.
+;; This is, obviously, considered to be an "internal" function. Which is a
+;; bit of shame really as it's a very handy thing that I think we'd normally
+;; want to be doing in elisp, when doing elisp-oriented things. So, rather
+;; than reinvent the wheel, I'll risk calling an internal and worry later
+;; when things get broken.
 
 ;;; Code:
-
-(require 'thingatpt)
 
 ;;;###autoload
 (defun expando-macro (&optional all)
@@ -27,7 +32,7 @@ By default `macroexpand' is used. Prefix a call to this function
 with \\[universal-argument] (or pass ALL as a non-nil value) to
 have `maxroexpand-all' be used."
   (interactive "P")
-  (let ((form (read (thing-at-point 'list t))))
+  (let ((form (elisp--preceding-sexp)))
     (with-current-buffer-window "*Expando Macro*" nil nil
       (emacs-lisp-mode)
       (pp (funcall (if all #'macroexpand-all #'macroexpand) form)))))
