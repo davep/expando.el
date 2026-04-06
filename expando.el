c179dc73a8abc9d1051c4e8ab3a6b36c7c2bc056
Dave Pearson <davep@davep.org>
2026-04-06 19:41:28 +0100

    :books: Drop the caveat about using an internal

--- expando.el
+++ expando.el
@@ -22,14 +22,7 @@
 ;;; Commentary:
 ;;
 ;; expando.el provides a simple tool for expanding macros into a different
-;; window so they can be quickly and easily checked and read.
-;;
-;; NOTE: This code makes use of `elisp--preceding-sexp' from elisp-mode.
-;; This is, obviously, considered to be an "internal" function. Which is a
-;; bit of shame really as it's a very handy thing that I think we'd normally
-;; want to be doing in elisp, when doing elisp-oriented things. So, rather
-;; than reinvent the wheel, I'll risk calling an internal and worry later
-;; when things get broken.
+;; window so that the expansion can be quickly and easily checked and read.
 
 ;;; Code:
 
